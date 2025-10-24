39fc32be5198b19037a4db241432e36158f4e99b
Mike Mozolin <mozolin@gmail.com>
2025-10-24 15:10:56 +0300

    led_indicator example

--- apps/mike_h2_tiny/main/led_config.h
+++ apps/mike_h2_tiny/main/led_config.h
@@ -27,7 +27,9 @@ GPIO_NUM_8  //-- RGB
 GPIO_NUM_13 //-- blue
 */
 #define ESP32_GPIO_LED_1         GPIO_NUM_8
+//-- is LED #1 an RGB LED or not
 #define ESP32_RGB_LED_1          true
 
 #define ESP32_GPIO_LED_2         GPIO_NUM_13
+//-- is LED #2 an RGB LED or not
 #define ESP32_RGB_LED_2          false
